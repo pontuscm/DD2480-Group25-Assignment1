7dd0e36460923a2f44d2a70295cac7198f3bda7a
pat672 <pat672@noreply.com>
2022-01-29 15:34:11 +0000

    Feature #15 - Create GeometryHelper class ahead of LIC 3 implementation
    GeometryHelper class will contain common mathematical functions that
    could be shared across multiple LIC class implementations.
    
    Also added a convenience method Coordinate::of

--- src/main/java/se/kth/dd2480/group25/assignment1/Coordinate.java
+++ src/main/java/se/kth/dd2480/group25/assignment1/Coordinate.java
@@ -1,28 +1,33 @@
 package se.kth.dd2480.group25.assignment1;
 
 public class Coordinate {
-    private double x;
-    private double y;
+	private double x;
+	private double y;
 
-    public Coordinate(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
+	public Coordinate(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 
-    public double getX() {
-        return x;
-    }
+	public double getX() {
+		return x;
+	}
 
-    public void setX(double x) {
-        this.x = x;
-    }
+	public void setX(double x) {
+		this.x = x;
+	}
 
-    public double getY() {
-        return y;
-    }
+	public double getY() {
+		return y;
+	}
 
-    public void setY(double y) {
-        this.y = y;
-    }
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public static Coordinate of(double x, double y) {
+		return new Coordinate(x, y);
+	}
+
 }
 
