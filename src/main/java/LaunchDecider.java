e76c495374b684396c2338967c8db7760c530d70
jolerus <henrik.jolerus@gmail.com>
2022-01-29 12:48:26 +0100

    Delete old LaunchDecider

--- src/main/java/LaunchDecider.java
+++
@@ -1,5 +0,0 @@
-public class LaunchDecider {
-	public static void main(String[] args) {
-		System.out.println("This application will decide whether to launch hypothetical ballistic countermeasures");
-	}
-}
