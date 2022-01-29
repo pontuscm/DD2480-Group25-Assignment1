80dd06052ed99638baf1b7004769b1786b2dc7b3
jolerus <henrik.jolerus@gmail.com>
2022-01-29 13:10:01 +0100

    Feature #26 - Create getters and setters for InputParameters and Coordinate (#27)
    * Delete old LaunchDecider
    
    * Feature #26 - Create getters and setters for InputParameters and Coordinate

--- src/main/java/se/kth/dd2480/group25/assignment1/Coordinate.java
+++ src/main/java/se/kth/dd2480/group25/assignment1/Coordinate.java
@@ -1,11 +1,28 @@
 package se.kth.dd2480.group25.assignment1;
 
 public class Coordinate {
-    double x;
-    double y;
+    private double x;
+    private double y;
 
     public Coordinate(double x, double y) {
         this.x = x;
         this.y = y;
     }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
+
--- src/main/java/se/kth/dd2480/group25/assignment1/InputParameters.java
+++ src/main/java/se/kth/dd2480/group25/assignment1/InputParameters.java
@@ -1,6 +1,26 @@
 package se.kth.dd2480.group25.assignment1;
 
 public class InputParameters {
+    private double length1;
+    private double radius1;
+    private double epsilon;
+    private double area1;
+    private int q_pts;
+    private int quads;
+    private double dist;
+    private int n_pts;
+    private int k_pts;
+    private int a_pts;
+    private int b_pts;
+    private int c_pts;
+    private int d_pts;
+    private int e_pts;
+    private int f_pts;
+    private int g_pts;
+    private double length2;
+    private double radius2;
+    private double area2;
+
     public InputParameters(double length1, double radius1, double epsilon, double area1, int q_pts, int quads, double dist, int n_pts, int k_pts, int a_pts, int b_pts, int c_pts, int d_pts, int e_pts, int f_pts, int g_pts, double length2, double radius2, double area2) {
         this.length1 = length1;
         this.radius1 = radius1;
@@ -23,23 +43,155 @@ public class InputParameters {
         this.area2 = area2;
     }
 
-    double length1;
-    double radius1;
-    double epsilon;
-    double area1;
-    int q_pts;
-    int quads;
-    double dist;
-    int n_pts;
-    int k_pts;
-    int a_pts;
-    int b_pts;
-    int c_pts;
-    int d_pts;
-    int e_pts;
-    int f_pts;
-    int g_pts;
-    double length2;
-    double radius2;
-    double area2;
+    public double getLength1() {
+        return length1;
+    }
+
+    public void setLength1(double length1) {
+        this.length1 = length1;
+    }
+
+    public double getRadius1() {
+        return radius1;
+    }
+
+    public void setRadius1(double radius1) {
+        this.radius1 = radius1;
+    }
+
+    public double getEpsilon() {
+        return epsilon;
+    }
+
+    public void setEpsilon(double epsilon) {
+        this.epsilon = epsilon;
+    }
+
+    public double getArea1() {
+        return area1;
+    }
+
+    public void setArea1(double area1) {
+        this.area1 = area1;
+    }
+
+    public int getQ_pts() {
+        return q_pts;
+    }
+
+    public void setQ_pts(int q_pts) {
+        this.q_pts = q_pts;
+    }
+
+    public int getQuads() {
+        return quads;
+    }
+
+    public void setQuads(int quads) {
+        this.quads = quads;
+    }
+
+    public double getDist() {
+        return dist;
+    }
+
+    public void setDist(double dist) {
+        this.dist = dist;
+    }
+
+    public int getN_pts() {
+        return n_pts;
+    }
+
+    public void setN_pts(int n_pts) {
+        this.n_pts = n_pts;
+    }
+
+    public int getK_pts() {
+        return k_pts;
+    }
+
+    public void setK_pts(int k_pts) {
+        this.k_pts = k_pts;
+    }
+
+    public int getA_pts() {
+        return a_pts;
+    }
+
+    public void setA_pts(int a_pts) {
+        this.a_pts = a_pts;
+    }
+
+    public int getB_pts() {
+        return b_pts;
+    }
+
+    public void setB_pts(int b_pts) {
+        this.b_pts = b_pts;
+    }
+
+    public int getC_pts() {
+        return c_pts;
+    }
+
+    public void setC_pts(int c_pts) {
+        this.c_pts = c_pts;
+    }
+
+    public int getD_pts() {
+        return d_pts;
+    }
+
+    public void setD_pts(int d_pts) {
+        this.d_pts = d_pts;
+    }
+
+    public int getE_pts() {
+        return e_pts;
+    }
+
+    public void setE_pts(int e_pts) {
+        this.e_pts = e_pts;
+    }
+
+    public int getF_pts() {
+        return f_pts;
+    }
+
+    public void setF_pts(int f_pts) {
+        this.f_pts = f_pts;
+    }
+
+    public int getG_pts() {
+        return g_pts;
+    }
+
+    public void setG_pts(int g_pts) {
+        this.g_pts = g_pts;
+    }
+
+    public double getLength2() {
+        return length2;
+    }
+
+    public void setLength2(double length2) {
+        this.length2 = length2;
+    }
+
+    public double getRadius2() {
+        return radius2;
+    }
+
+    public void setRadius2(double radius2) {
+        this.radius2 = radius2;
+    }
+
+    public double getArea2() {
+        return area2;
+    }
+
+    public void setArea2(double area2) {
+        this.area2 = area2;
+    }
 }
