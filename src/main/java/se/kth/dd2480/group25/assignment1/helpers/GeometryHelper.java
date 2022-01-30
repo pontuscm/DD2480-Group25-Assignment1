7c68f4780a073b70479e3e0a1ae7bdeb9056d161
Xinyi Wan <xinyiwan@kth.se>
2022-01-30 13:55:14 +0100

    Added two math methods
    1. added isCollinear() method to check if three points are collinear.
    
    2. added distanceBetween() method to calculate the distance between 2 points.

--- src/main/java/se/kth/dd2480/group25/assignment1/helpers/GeometryHelper.java
+++ src/main/java/se/kth/dd2480/group25/assignment1/helpers/GeometryHelper.java
@@ -21,4 +21,36 @@ public final class GeometryHelper {
                             second.getX() * (third.getY() - first.getY()) +
                             third.getX() * (first.getY() - second.getY())) / 2;
     }
+
+     /**
+     * Check if three points are collinear
+     *
+     * @return ture or false
+     */
+
+     public static boolean isCollinear(Coordinate a, Coordinate b, Coordinate c) {
+        double slopeAB = (b.getY() - a.getY()) / (b.getX() - a.getX());
+        double slopeBC = (c.getY() - b.getY()) / (c.getX() - b.getX());
+        double slopeAC = (c.getY() - a.getY()) / (c.getX() - a.getX());
+
+        if( Double.compare(slopeAB, slopeAC) == 0 ||
+            Double.compare(slopeBC, slopeAC) == 0 ||
+            Double.compare(slopeAB, slopeBC) == 0){
+                return true;
+            }
+        return false;
+     }
+
+    /**
+     * Calculate the distance between point a and b
+     * 
+     * @return the distance between point a and b
+     */
+
+    public static double distanceBetween(Coordinate a, Coordinate b){
+        double xDis = a.getX() - b.getX();
+        double yDis = a.getY() - b.getY();
+        return Math.sqrt((xDis * xDis) + (yDis * yDis));
+    }
+    
 }
