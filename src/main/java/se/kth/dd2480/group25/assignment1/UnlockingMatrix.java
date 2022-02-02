adf5ecea0f7dee7bd927eaf8762513c4992a99f2
Emilia <emiros@kth.se>
2022-02-02 18:52:56 +0100

    Feature #24 - Implement basic UnlockingMatrix class

--- src/main/java/se/kth/dd2480/group25/assignment1/UnlockingMatrix.java
+++ src/main/java/se/kth/dd2480/group25/assignment1/UnlockingMatrix.java
@@ -1,11 +1,24 @@
 package se.kth.dd2480.group25.assignment1;
 
-import java.util.List;
+import java.util.*;
 
 public class UnlockingMatrix {
+    private Boolean[][] matrix = new Boolean[15][15];
 
     public List<Boolean> applyPreliminaryUnlockingVector(List<Boolean> preliminaryUnlockingVector) {
-        // TODO: Return the Final unlocking vector
-        return null;
+        List<Boolean> unlockingVector = new ArrayList<>();
+
+        for (int i = 0; i < preliminaryUnlockingVector.size(); i++) {
+            if (preliminaryUnlockingVector.get(i)) {
+                unlockingVector.add(Collections.frequency(Arrays.asList(this.matrix[i]), false) == 0);
+            }
+            else { unlockingVector.add(true); } //if false, the condition isn't relevant and the corresponding value is unconditionally true
+        }
+        return unlockingVector;
     }
+
+    public void setIndex(int i, int j, Boolean value) {
+        this.matrix[i][j] = value;
+    }
+
 }
