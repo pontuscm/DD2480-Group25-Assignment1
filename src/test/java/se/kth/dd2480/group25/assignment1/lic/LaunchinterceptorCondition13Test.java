6dacd9e99c61479dae856306f6f171533fb81965
Xinyi Wan <xinyiwan@kth.se>
2022-02-02 22:43:41 +0100

    #51  Clear up the codes of test LIC13

--- src/test/java/se/kth/dd2480/group25/assignment1/lic/LaunchinterceptorCondition13Test.java
+++ src/test/java/se/kth/dd2480/group25/assignment1/lic/LaunchinterceptorCondition13Test.java
@@ -31,13 +31,21 @@ public class LaunchinterceptorCondition13Test {
 
     static Stream<Arguments> validInputProvider() {
         return Stream.of(
-            arguments(List.of(Coordinate.of(0, 0),Coordinate.of(6, 5),Coordinate.of(0, 1),Coordinate.of(6, 5),Coordinate.of(1, 1)),0.7,0.6,1,1)
+            arguments(List.of(Coordinate.of(0, 0),
+                              Coordinate.of(6, 5),
+                              Coordinate.of(0, 1),
+                              Coordinate.of(6, 5),
+                              Coordinate.of(1, 1)),
+                              0.7,0.6,1,1)  // circumcircle radius is around 0.707
         );
     }
 
     @Test
     void shouleRejectIfTooFewCoordinates() {
-        List<Coordinate> coordinates = List.of(Coordinate.of(0, 0),Coordinate.of(0, 1),Coordinate.of(1, 1),Coordinate.of(3, 5)); 
+        List<Coordinate> coordinates = List.of(Coordinate.of(0, 0),
+                                               Coordinate.of(0, 1),
+                                               Coordinate.of(1, 1),
+                                               Coordinate.of(3, 5)); 
         parameters.setRadius1(0.7);
         parameters.setRadius2(0.7);
 
@@ -48,19 +56,28 @@ public class LaunchinterceptorCondition13Test {
 
     @Test
     void shouleRejectIfWrongInterveneNumber() {
-        List<Coordinate> coordinates = List.of(Coordinate.of(0, 0),Coordinate.of(4,7),Coordinate.of(0, 1),Coordinate.of(3, 5),Coordinate.of(1, 1),Coordinate.of(0, 0)); 
+        List<Coordinate> coordinates = List.of(Coordinate.of(0, 0),
+                                               Coordinate.of(4, 7),
+                                               Coordinate.of(0, 1),
+                                               Coordinate.of(3, 5),
+                                               Coordinate.of(1, 1),
+                                               Coordinate.of(0, 0)); 
         parameters.setRadius1(1);
         parameters.setRadius2(0.7);
 
         parameters.setA_pts(1);
-        parameters.setB_pts(2);
+        parameters.setB_pts(-2);
         assertFalse(condition.evaluate(coordinates, parameters));
     }
 
 
     @Test
     void shouldRejectIfTooSmallRadius() {
-        List<Coordinate> coordinates = List.of(Coordinate.of(0, 0),Coordinate.of(1, 0),Coordinate.of(0, 1),Coordinate.of(1, 2),Coordinate.of(1, 1)); 
+        List<Coordinate> coordinates = List.of(Coordinate.of(0, 0),
+                                               Coordinate.of(1, 0),
+                                               Coordinate.of(0, 3),
+                                               Coordinate.of(1, 2),
+                                               Coordinate.of(4, 0)); // circumcircle radius is 2.5
         parameters.setRadius1(3);
         parameters.setRadius2(3);
 
