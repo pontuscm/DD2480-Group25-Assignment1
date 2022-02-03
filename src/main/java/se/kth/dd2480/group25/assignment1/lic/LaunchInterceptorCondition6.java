87bdc4aa981c60aac69122422d8a731138f04e3b
pat672 <pat672@noreply.com>
2022-02-03 00:25:19 +0000

    Feature #55 - Added initial integrations tests
    Introduced two cases, one positive and negative each. These could be
    refined to encompass more of the complexity of the problem space so
    should be seen as the first step in an iterative testing process.

---
+++ src/main/java/se/kth/dd2480/group25/assignment1/lic/LaunchInterceptorCondition6.java
@@ -0,0 +1,15 @@
+package se.kth.dd2480.group25.assignment1.lic;
+
+import se.kth.dd2480.group25.assignment1.Coordinate;
+import se.kth.dd2480.group25.assignment1.InputParameters;
+
+import java.util.List;
+
+public class LaunchInterceptorCondition6 implements LaunchInterceptorCondition {
+
+    @Override
+    public boolean evaluate(List<Coordinate> coordinates, InputParameters inputParameters) {
+        return true; // Placeholder implementation to allow integration tests to run properly
+    }
+
+}
